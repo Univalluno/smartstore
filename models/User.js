f2dc40f62ba747c6f92e26218e9b7f969b355da1
Univalluno <146486793+Univalluno@users.noreply.github.com>
2025-11-25 22:35:39 -0500

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  mfaSecret: String,
+  mfaEnabled: { type: Boolean, default: false }
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = function(pwd) {
+  return bcrypt.compare(pwd, this.password);
+};
+
+export default mongoose.model('User', userSchema);
